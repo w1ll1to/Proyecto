f9913df80e8a22ef706f6f5b22baf3d4a1405a4a
Wiliams Segura Alvarez <w1ll1to_23oct_95@hotmail.com>
2017-08-22 21:21:05 -0500

    paginas php creadas en clase enlazadas a las paginas html

---
+++ pagina.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title>Mi Pagina PHP</title>
+    </head>
+    <body>
+        <h1>Mi pagina en PHP</h1>
+        <?php
+        	$variable2 = 1;
+        	for ($variable2=1; $variable2 < 20; $variable2++){
+        		echo $variable2;
+        		echo "<br>";
+        	};
+        	echo "<br>";
+        	echo "<br>";
+        	echo 'Nuestra primera p&aacute;gina PHP';
+        	echo "<br>";
+        	echo "<br>";
+        	$variable1 = 1;
+        	echo $variable1;
+        ?>
+    </body>
+</html>
---
+++ prueba1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Wiliams Segura Alvarez</title>
+</head>
+<body>
+    <h1>Mi pagina en PHP</h1>
+    <?PHP
+    $variable1=1;
+    echo "$variable1";
+    echo '<br>';
+    echo '<p>Esta es una pagina en php realizada en clase</p>';
+    echo "<br>";
+    echo "<p>Mensaje mensaje mensaje</p>";
+    echo"<br>";
+    echo "<p>Wiliams Segura Alvarez</p>";
+    echo"<br>";
+    echo "<p>Esta es una pagina en php realizada en clase</p>";
+    echo"<br>";
+    echo "<p>Hola </p>";
+    ?>
+</body>
+</html>
